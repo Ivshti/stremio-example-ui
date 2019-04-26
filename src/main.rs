f086c2b94ffd2b848dac89a8e50ed4bae79d8035
Ivo Georgiev <ivo@strem.io>
2019-04-26 16:08:46 +0300

    gfx wip

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,22 @@ use serde::de::DeserializeOwned;
 use serde::Serialize;
 use std::cell::RefCell;
 use std::rc::Rc;
+use std::thread;
+use std::sync::mpsc::channel;
+
+// for now, we will spawn conrod in a separate thread and communicate via channels
+// otherwise, we may find a better solution here:
+// https://github.com/tokio-rs/tokio-core/issues/150
 
 fn main() {
+    //let (actionTx, actionRx) = channel();
+    //let (eventTx, eventRx) = channel();
+
     let container = Rc::new(RefCell::new(Container::with_reducer(
         CatalogGrouped::new(),
         &catalogs_reducer,
     )));
-    #[derive(Debug)]
+    #[derive(Debug, Clone)]
     enum ContainerId {
         Board,
     };
@@ -24,21 +33,139 @@ fn main() {
             Box::new(AddonsMiddleware::<Env>::new()),
         ],
         vec![(ContainerId::Board, container.clone())],
-        Box::new(|_event| {
-            if let Event::NewState(_) = _event {
-                dbg!(_event);
+        Box::new(move |ev| {
+            if let Event::NewState(_) = ev {
+                dbg!(ev);
             }
+            //eventTx.send(ev).map_err(|_| ());
         }),
     ));
+    
+    // Spawn the UI
+    let handler = thread::spawn(run_ui);
 
+    // @TODO channel that's listening for actions
     run(lazy(move || {
         // this is the dispatch operation
         let action = &Action::Load(ActionLoad::CatalogGrouped { extra: vec![] });
         muxer.dispatch(action);
         future::ok(())
     }));
+    handler.join().unwrap();
 }
 
+
+const WIN_H: u32 = 400;
+const WIN_W: u32 = 400;
+use gfx::Device;
+
+const CLEAR_COLOR: [f32; 4] = [0.2, 0.2, 0.2, 1.0];
+
+type DepthFormat = gfx::format::DepthStencil;
+
+// A wrapper around the winit window that allows us to implement the trait necessary for enabling
+// the winit <-> conrod conversion functions.
+struct WindowRef<'a>(&'a winit::Window);
+
+// Implement the `WinitWindow` trait for `WindowRef` to allow for generating compatible conversion
+// functions.
+impl<'a> conrod_winit::WinitWindow for WindowRef<'a> {
+    fn get_inner_size(&self) -> Option<(u32, u32)> {
+        winit::Window::get_inner_size(&self.0).map(Into::into)
+    }
+    fn hidpi_factor(&self) -> f32 {
+        winit::Window::get_hidpi_factor(&self.0) as _
+    }
+}
+
+fn run_ui() {
+    // Builder for window
+    let builder = glutin::WindowBuilder::new()
+        .with_title("Conrod with GFX and Glutin")
+        .with_dimensions((WIN_W, WIN_H).into());
+
+    let context = glutin::ContextBuilder::new()
+        .with_multisampling(4);
+
+    let mut events_loop = winit::EventsLoop::new();
+
+    // Initialize gfx things
+    let (window, mut device, mut factory, rtv, _) =
+        gfx_window_glutin::init::<conrod_gfx::ColorFormat, DepthFormat>(builder, context, &events_loop).unwrap();
+    let mut encoder: gfx::Encoder<_, _> = factory.create_command_buffer().into();
+
+    let mut renderer = conrod_gfx::Renderer::new(&mut factory, &rtv, window.get_hidpi_factor() as f64).unwrap();
+
+    // Create Ui and Ids of widgets to instantiate
+    let mut ui = conrod_core::UiBuilder::new([WIN_W as f64, WIN_H as f64])
+        .build();
+
+    let mut image_map = conrod_core::image::Map::new();
+
+    'main: loop {
+        // If the window is closed, this will be None for one tick, so to avoid panicking with
+        // unwrap, instead break the loop
+        let (win_w, win_h): (u32, u32) = match window.get_inner_size() {
+            Some(s) => s.into(),
+            None => break 'main,
+        };
+
+        let dpi_factor = window.get_hidpi_factor() as f32;
+
+        if let Some(primitives) = ui.draw_if_changed() {
+            let dims = (win_w as f32 * dpi_factor, win_h as f32 * dpi_factor);
+
+            //Clear the window
+            renderer.clear(&mut encoder, CLEAR_COLOR);
+
+            renderer.fill(&mut encoder,dims,dpi_factor as f64,primitives,&image_map);
+
+            renderer.draw(&mut factory,&mut encoder,&image_map);
+
+            encoder.flush(&mut device);
+            window.swap_buffers().unwrap();
+            device.cleanup();
+        }
+
+        let mut should_quit = false;
+        events_loop.poll_events(|event|{
+
+            // Convert winit event to conrod event, requires conrod to be built with the `winit` feature
+            if let Some(event) = conrod_winit::convert_event(event.clone(), &WindowRef(window.window())) {
+                ui.handle_event(event);
+            }
+
+            // Close window if the escape key or the exit button is pressed
+            match event {
+                winit::Event::WindowEvent{event, .. } =>
+                    match event {
+                        winit::WindowEvent::KeyboardInput{ input: winit::KeyboardInput{ virtual_keycode: Some(winit::VirtualKeyCode::Escape),..}, ..} |
+                        winit::WindowEvent::CloseRequested => should_quit = true,
+                        winit::WindowEvent::Resized(logical_size) => {
+                            let hidpi_factor = window.get_hidpi_factor();
+                            let physical_size = logical_size.to_physical(hidpi_factor);
+                            window.resize(physical_size);
+                            let (new_color, _) = gfx_window_glutin::new_views::<conrod_gfx::ColorFormat, DepthFormat>(&window);
+                            renderer.on_resize(new_color);
+                        }
+                        _ => {},
+                    },
+                _ => {},
+            }
+        });
+        if should_quit {
+            break 'main;
+        }
+
+        // Update widgets if any event has happened
+        if ui.global_input().events().next().is_some() {
+            let mut ui = ui.set_widgets();
+        }
+    }
+}
+
+
+// Define the environment
 struct Env {}
 impl Environment for Env {
     fn fetch_serde<IN, OUT>(in_req: Request<IN>) -> EnvFuture<Box<OUT>>
